24ba90b7b151effdfc7e45af1f17273ac793dac6
Vinodh Kanna RJ (DHL IT Services) <vinodh_kanna.rj@dhl.com>
2020-12-21 10:15:14 +0530

    increase time

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,12 @@
-import React, { useCallback, useEffect, useReducer } from 'react';
+import React from 'react';
 import './App.scss';
 import Header from './components/Header';
-import dataReducer, { initialState } from './components/reducer';
-import ACTIONS from './components/reducer/actions';
 import Tables from './components/Table';
 import BooksContext from './context';
+import useWebSocket from './useWebSocket';
 
 const App = () => {
-  const booksStore = localStorage.getItem('booksStore');
-  const [state, dispatch] = useReducer(
-    dataReducer, booksStore ? JSON.parse(booksStore) : initialState,
-  );
-
-  const saveStore = useCallback(() => localStorage.setItem('booksStore', JSON.stringify(state)), [state]);
-
-  useEffect(() => {
-    const ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
-    let i = 0;
-    ws.onopen = () => {
-      ws.send(JSON.stringify({
-        event: 'subscribe',
-        channel: 'book',
-        symbol: 'tBTCUSD',
-      }));
-    };
-    ws.onmessage = ({ data }) => {
-      const msg = JSON.parse(data);
-      if (msg.event) return;
-      if (msg[1] && msg[1][0] && Array.isArray(msg[1][0])) {
-        dispatch([ACTIONS.INITIAL, msg[1]]);
-      } else {
-        const [, count, amount] = msg[1];
-        i += 1;
-        if (count > 0) {
-          setTimeout(() => {
-            dispatch([amount > 0 ? ACTIONS.UPDATE_BIDS : ACTIONS.UPDATE_ASKS, msg[1]]);
-          }, i * 100);
-        } else {
-          setTimeout(() => {
-            dispatch([amount > 0 ? ACTIONS.DELETE_BIDS : ACTIONS.DELETE_ASKS, msg[1]]);
-          }, i * 100);
-        }
-      }
-    };
-
-    window.addEventListener('unload', saveStore, false);
-    return () => {
-      try {
-        window.removeEventListener('unload', saveStore, false);
-        ws.close();
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.error(e, 'error in closing websocket');
-      }
-    };
-  }, []);
-
+  const state = useWebSocket();
   return (
     <div className="App">
       <Header />
